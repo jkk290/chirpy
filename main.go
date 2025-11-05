98c537b752c8dcac5790c44c09ec7ed988dfbb21
jkk290 <jared@kuromoto.dev>
2025-11-05 06:21:19 -1000

    Add custom handler for checking if server is ready for requests

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 	srv := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
@@ -19,4 +20,10 @@ func main() {
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 
+}
+
+func handlerReadiness(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
